65beb303ee049d097f55ad28e3db0ac3ae448b9c
Clara Aldegunde Mantteca <118371091+claraldegunde@users.noreply.github.com>
2024-01-26 16:51:04 +0000

    Add files via upload

--- CIGALE_results.py
+++ CIGALE_results.py
@@ -7,9 +7,11 @@ Created on Tue Jan  9 18:23:19 2024
 """
 import pandas as pd 
 import numpy as np
+import matplotlib.pyplot as plt
+#%%
 
 #Load data 
-data = pd.read_csv('/Users/claraaldegundemanteca/Desktop/CIGALE/cigale-v2022.1/out/results.txt')
+data = pd.read_csv('/Users/claraaldegundemanteca/Desktop/CIGALE/cigale-v2022.1/out/results_170124.txt')
 
 #Create dataframe
 headers = data.columns[0]
@@ -37,3 +39,38 @@ df = pd.DataFrame(data = values_list, columns = headers_list) #Dataframe created
 
 
 #%% 
+
+redshifts = np.array(df['bayes.universe.redshift'])
+chi2 = np.array(df['best.chi_square'])
+
+#%% impose condition on chi2
+
+accept = (df['best.chi_square']<=0.1)
+#%% z distribution
+
+plt.hist(redshifts, bins= 20, color='k', alpha = 0.7)
+plt.grid(alpha=0.7)
+plt.ylabel('Frequency')
+plt.xlabel('z')
+plt.title('Photometric redshift distribution (from CIGALE)')
+
+#%% chi2 distribution
+
+plt.figure()
+plt.hist(chi2, bins= 20, color='k', alpha = 0.7)
+plt.grid(alpha=0.7)
+plt.ylabel('Frequency')
+plt.xlabel('$\chi^2$')
+plt.title('$\chi^2$ distribution ')
+
+#%% chi2 against redshift 
+
+chi2 = np.array(chi2)
+redshift = np.array(chi2)
+
+plt.figure()
+plt.scatter(redshifts[accept], chi2[accept],color='k', alpha = 0.7)
+plt.grid(alpha=0.7)
+plt.ylabel('$\chi^2$')
+plt.xlabel('z')
+plt.title('$\chi^2$ vs z ')
